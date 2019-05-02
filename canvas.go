86fbb417a9e14545bd390c5c7b9a34fcae73addd
Alexander F. Rødseth <rodseth@gmail.com>
2019-05-02 14:34:49 +0200

    Improve the canvas functionality

--- canvas.go
+++ canvas.go
@@ -11,28 +11,81 @@ type Char struct {
 	s      string
 }
 
-type Canvas [80 * 25]Char
-
-func NewCanvas() *Canvas {
-	var c Canvas
-	return &c
+type Canvas struct {
+	w     uint
+	h     uint
+	chars []Char
 }
 
-func setxy(x, y int) {
-	Set("Cursor Home", map[string]string{"{ROW}": strconv.Itoa(y), "{COLUMN}": strconv.Itoa(x)})
+func NewCanvas(w, h int) *Canvas {
+	var err error
+	c := &Canvas{}
+	c.w, c.h, err = TermSize()
+	if err != nil {
+		c.w = 80
+		c.h = 25
+	}
+	c.chars = make([]Char, c.w*c.h)
+	return c
 }
 
-func down(n int) {
-	Set("Cursor Down", map[string]string{"{COUNT}": strconv.Itoa(n)})
+// Move cursor to the given position
+func SetXY(x, y uint) {
+	Set("Cursor Home", map[string]string{"{ROW}": strconv.Itoa(int(y)), "{COLUMN}": strconv.Itoa(int(x))})
+}
+
+// Move the cursor down
+func Down(n uint) {
+	Set("Cursor Down", map[string]string{"{COUNT}": strconv.Itoa(int(n))})
+}
+
+// Move the cursor up
+func Up(n uint) {
+	Set("Cursor Up", map[string]string{"{COUNT}": strconv.Itoa(int(n))})
+}
+
+// Move the cursor to the right
+func Right(n uint) {
+	Set("Cursor Forward", map[string]string{"{COUNT}": strconv.Itoa(int(n))})
+}
+
+// Move the cursor to the left
+func Left(n uint) {
+	Set("Cursor Backward", map[string]string{"{COUNT}": strconv.Itoa(int(n))})
+}
+
+func (c *Canvas) Reset() {
+	Do("Reset Device")
+}
+
+func (c *Canvas) Clear() {
+	Do("Erase Screen")
+}
+
+func (c *Canvas) SetLineWrap(enable bool) {
+	if enable {
+		Do("Enable Line Wrap")
+	} else {
+		Do("Disable Line Wrap")
+	}
+}
+
+func (c *Canvas) SetCursor(enable bool) {
+	// Thanks https://rosettacode.org/wiki/Terminal_control/Hiding_the_cursor#Escape_code
+	if enable {
+		fmt.Print("\033[?25h")
+	} else {
+		fmt.Print("\033[?25l")
+	}
 }
 
 func (c *Canvas) Draw() {
 	// TODO: Only draw the characters that have changed since last draw
-	for y := 0; y < 25; y++ {
-		for x := 0; x < 80; x++ {
-			ch := (*c)[y*80+x]
+	for y := uint(0); y < c.h; y++ {
+		for x := uint(0); x < c.w; x++ {
+			ch := (*c).chars[y*c.w+x]
 			if ch.s != "" {
-				setxy(x, y)
+				SetXY(x, y)
 				if ch.bright {
 					fmt.Print(AttributeAndColor("Bright", ch.fg) + ch.s + NoColor())
 				} else {
@@ -41,14 +94,14 @@ func (c *Canvas) Draw() {
 			}
 		}
 	}
-	setxy(79, 24)
+	SetXY(c.w-1, c.h-1)
 }
 
-func (c *Canvas) Plot(x, y int, s string) {
-	(*c)[y*80+x].s = s
+func (c *Canvas) Plot(x, y uint, s string) {
+	(*c).chars[y*c.w+x].s = s
 }
 
-func (c *Canvas) PlotC(x, y int, fg, s string) {
-	(*c)[y*80+x].s = s
-	(*c)[y*80+x].fg = fg
+func (c *Canvas) PlotC(x, y uint, fg, s string) {
+	(*c).chars[y*c.w+x].s = s
+	(*c).chars[y*c.w+x].fg = fg
 }
--- cmd/canvas/main.go
+++ cmd/canvas/main.go
@@ -2,11 +2,19 @@ package main
 
 import (
 	"github.com/xyproto/vt100"
+	"time"
 )
 
 func main() {
-	c := vt100.NewCanvas()
+	c := vt100.NewCanvas(80, 25)
+	c.Reset()
+	c.Clear()
+	c.SetCursor(false)
+	c.SetLineWrap(false)
 	c.Plot(10, 10, "!")
 	c.PlotC(20, 20, "Blue", "?")
 	c.Draw()
+	time.Sleep(time.Second * 2)
+	c.SetLineWrap(true)
+	c.SetCursor(true)
 }
