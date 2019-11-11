1870d0f260724cad01206481581965bd03cd7a7b
Alexander F. Rødseth <rodseth@gmail.com>
2019-11-11 14:08:17 +0100

    Add a function for blocking until a key is pressed

---
+++ cmd/keyblock/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"github.com/xyproto/vt100"
+)
+
+func main() {
+	escCount := 0
+	tty, err := vt100.NewTTY()
+	if err != nil {
+		panic(err)
+	}
+	for {
+		key := tty.KeyBlock()
+		if key != 0 {
+			fmt.Println(key)
+		}
+		if key == 27 {
+			if escCount == 0 {
+				fmt.Println("Press ESC again to exit")
+			} else {
+				fmt.Println("bye!")
+			}
+			escCount++
+		}
+		if escCount > 1 {
+			break
+		}
+	}
+	tty.Close()
+}
