5727c0f9a7e755ef035fb7210d835df6e0ac4dfb
Alexander F. Rødseth <rodseth@gmail.com>
2023-08-01 14:12:31 +0200

    Add benchmarks

--- colors_test.go
+++ colors_test.go
@@ -5,6 +5,8 @@ import (
 	"testing"
 )
 
+var ac = NewAttributeColor("Bright", "Blue")
+
 func TestBackground(t *testing.T) {
 	if BackgroundBlue.String() != Blue.Background().String() {
 		fmt.Println("BLUE BG IS NOT BLUE BG")
@@ -28,5 +30,89 @@ func TestInts(t *testing.T) {
 			fmt.Println("NO")
 			t.Fail()
 		}
+	}
+}
+
+func BenchmarkNewAttributeColor(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		NewAttributeColor("Bright", "Blue")
+	}
+}
+
+func BenchmarkHead(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		ac.Head()
+	}
+}
+
+func BenchmarkTail(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		ac.Tail()
+	}
+}
+
+func BenchmarkBackground(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		ac.Background()
+	}
+}
+
+func BenchmarkString(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		ac.String()
+	}
+}
+
+func BenchmarkStartStop(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		ac.StartStop("test")
+	}
+}
+
+func BenchmarkGet(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		ac.Get("test")
+	}
+}
+
+func BenchmarkStart(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		ac.Start("test")
+	}
+}
+
+func BenchmarkStop(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		ac.Stop("test")
+	}
+}
+
+func BenchmarkCombine(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		ac.Combine(NewAttributeColor("Red"))
+	}
+}
+
+func BenchmarkBright(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		ac.Bright()
+	}
+}
+
+func BenchmarkInts(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		ac.Ints()
+	}
+}
+
+func BenchmarkS2b(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		s2b("Blue")
+	}
+}
+
+func BenchmarkB2s(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		b2s(34) // 34 corresponds to "Blue" in s2b function
 	}
 }
